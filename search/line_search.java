5f38a1f585422a6b400ac73c8ae4694180b2f069
AMEMIYA_KENJI_MAC <hitohahito@hotmail.co.jp>
2020-04-05 22:01:27 +0900

    binary

---
+++ search/line_search.java
@@ -0,0 +1,18 @@
+class LineSearch {
+    public static void main(String[] args) {
+
+        int a[] = {10,3,1,4,2};
+        int searchValue = 4;
+        int findID = -1;
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == searchValue) {
+                findID = i;
+                break;
+            }
+        }
+
+        System.out.println("見つかった番号 = " + findID);
+    }
+
+}
