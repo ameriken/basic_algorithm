415af784a42523c2bd41da569e0473d6369870d7
AMEMIYA_KENJI_MAC <hitohahito@hotmail.co.jp>
2020-04-05 20:08:02 +0900

    swap

---
+++ easy/swap.java
@@ -0,0 +1,16 @@
+class Swap {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+        int t;
+
+        t = a;
+        a = b;
+        b = t;
+
+
+        System.out.println("a = " + a + " b = " + b);
+    }
+
+}
